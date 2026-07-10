2117ca746d4e6a4d605b280cb451550b31d27fe2
Wallet Watcher Pro <dev@walletwatcher.pro>
2026-07-10 08:29:55 +0700

    Try webDir: '.' again from working directory

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.walletwatcher.pro',
   appName: 'Wallet Watcher Pro',
-  webDir: '.',
+  webDir: 'wallet-watcher-pro',
   server: {
     androidScheme: 'https',
     cleartext: false,
